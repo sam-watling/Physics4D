432d7f3255245d047197f1d3849d9564749c26c6
Sam Watling <78026467+sam-watling@users.noreply.github.com>
2022-03-10 11:39:00 +0000

    demo shader

--- shaderToyShader.fs
+++ shaderToyShader.fs
@@ -5,9 +5,22 @@
 vec4 lightPos = vec4(2, 1, 0,4);
 
 vec4 spherePos = vec4(1.,1.,5.,2.5);
-vec4 sphere2Pos = vec4(1.,1.,10.,3.1);
+vec4 sphere2Pos = vec4(0.,1.,6.,3.1);
 vec4 sphere3Pos = vec4(1.,1.,3.,3.2);
+vec4 tetPos = vec4(-0.,0.,5.,3.);
 vec4 cubePos= vec4(-3.,1.,4.,2.5);
+
+mat4 rotate(float theta)
+{
+    float c = cos(theta);
+    float s = sin(theta);
+    return mat4(
+           vec4(c,0,-s,0),
+           vec4(0,c,0,-s),
+           vec4(s,0,c,0),
+           vec4(0,s,0,c));
+}
+
 
 float sd4Sphere(vec4 p, float r) 
 {
@@ -16,20 +29,17 @@ float sd4Sphere(vec4 p, float r)
 
 float sd4Cube(vec4 p, vec4 b)
 {
+    p*= rotate(iTime);
     vec4 q = abs(p) - b;
     return min(max(max(max(q.z,q.w),q.y),q.x),0.) + length(max(q,0.));
 }
 
-vec2 opU( vec2 d1, vec2 d2 )
-{
-    return (d1.x<d2.x) ? d1 : d2;
-}
 
-vec2 map(vec4 pos)
+float map(vec4 pos)
 {
-    vec2 res = opU(vec2(sd4Sphere(pos-spherePos,1.0),1.0),vec2(sd4Cube(pos-cubePos,vec4(.7)),1.0));
-         res = opU(res, vec2(min(sd4Sphere(pos-sphere2Pos,1.0),sd4Sphere(pos-sphere3Pos,0.5)),1.0));
-         res = opU(res, vec2(sd4Sphere(pos-sphere3Pos,0.5),1.0));
+    float res = min((sd4Sphere(pos-spherePos,1.0)),sd4Cube(pos-cubePos,vec4(.7)));
+         res = min(res, (min(sd4Sphere(pos-sphere2Pos,1.0),sd4Sphere(pos-sphere3Pos,0.5))));
+         res = min(res, (sd4Sphere(pos-sphere3Pos,0.5)));
     return res;
 }
 
@@ -40,7 +50,7 @@ float rayMarch(vec4 rayO, vec4 rayD)
     for(int i=0; i<MAX_MARCH_STEPS; i++)
     {
         vec4 p = rayO + rayD*distO;
-        float distS = map(p).x;
+        float distS = map(p);
         distO += distS;
         if(distO>MAX_MARCH_DIST||distS<HIT_DIST) break;
     }
@@ -48,14 +58,14 @@ float rayMarch(vec4 rayO, vec4 rayD)
 }
 
 vec4 GetNormal(vec4 p) {
-	float d = map(p).x;
+	float d = map(p);
     vec2 e = vec2(.01, 0);
     
     vec4 n = d - vec4(
-        map(p-e.xyyy).x,
-        map(p-e.yxyy).x,
-        map(p-e.yyxy).x,
-        map(p-e.yyyx).x);
+        map(p-e.xyyy),
+        map(p-e.yxyy),
+        map(p-e.yyxy),
+        map(p-e.yyyx));
     
     return normalize(n);
 }
@@ -78,15 +88,16 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
 
     vec3 col = vec3(0);
-    float w = 3.+sin(iTime);
-    vec4 ro = vec4(0, 1, 0,w);
-    vec4 rd = normalize(vec4(uv.x, uv.y, 1, 0));
+    float w = 2.7+sin(iTime);
+    vec4 ro = vec4(0, 1, -2,w-3.);
+    vec4 rd = normalize(vec4(uv.x, uv.y, 1, 0.6));
 
     float d = rayMarch(ro, rd);
     
     vec4 p = ro + rd * d;
     
     float dif = GetLight(p);
+    if(d<42.)dif+=0.2;
     col = vec3(dif);
     
     col = pow(col, vec3(.4545));	// gamma correction
